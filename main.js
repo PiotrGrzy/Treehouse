321365be2ae6e8768fd608223b8a8a67ccb3ea51
unknown <piotr.grzymowicz1@gmail.com>
2020-01-20 17:53:40 +0100

    aprroved

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 //Mobile NAVIGATION and Burger button events
 
-const burger = document.querySelector(".burger");
-const mobileNav = document.querySelector(".mobile");
-const mobileLinks = document.querySelectorAll(".mobile__link");
+const burger = document.querySelector('.burger');
+const mobileNav = document.querySelector('.mobile');
+const mobileLinks = document.querySelectorAll('.mobile__link');
 
 const toggleActive = () => {
-  burger.classList.toggle("burger--active");
-  mobileNav.classList.toggle("mobile--active");
+  burger.classList.toggle('burger--active');
+  mobileNav.classList.toggle('mobile--active');
 };
 
-burger.addEventListener("click", toggleActive);
-mobileLinks.forEach(link => link.addEventListener("click", toggleActive));
+burger.addEventListener('click', toggleActive);
+mobileLinks.forEach(link => link.addEventListener('click', toggleActive));
 
 // Animation Library
 
 AOS.init({
-  delay: 200, // values from 0 to 3000, with step 50ms
+  delay: 100, // values from 0 to 3000, with step 50ms
   duration: 800 // values from 0 to 3000, with step 50ms
 });
